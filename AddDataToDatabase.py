2b62b4a33fc0cc7e99dbbe0a721eba73a02e7edf
AasthaMasalawala08 <167021271+AasthaMasalawala08@users.noreply.github.com>
2024-04-14 22:58:04 +0530

    Add files via upload

---
+++ AddDataToDatabase.py
@@ -0,0 +1,75 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL':"https://faceattendancerealtime-334c0-default-rtdb.firebaseio.com/"
+})
+
+ref = db.reference('Students')
+
+data = {
+ "321654":
+        {
+            "name": "Murtaza Hassan",
+            "major": "Robotics",
+            "starting_year": 2017,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 4,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "852741":
+        {
+            "name": "Emly Blunt",
+            "major": "Economics",
+            "starting_year": 2021,
+            "total_attendance": 12,
+            "standing": "B",
+            "year": 1,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "963852":
+        {
+            "name": "Elon Musk",
+            "major": "Physics",
+            "starting_year": 2020,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 2,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "1":
+        {
+            "name": "Aastha",
+            "major": "Maths",
+            "starting_year": 2024,
+            "total_attendance": 0,
+            "standing": "G",
+            "year": 2,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "2":
+        {
+        "name": "Bhagya",
+        "major": "chemistry",
+        "starting_year": 2024,
+        "total_attendance": 2,
+        "standing": "G",
+        "year": 2,
+        "last_attendance_time": "2022-12-11 00:54:34"
+    }
+}
+
+
+for key, value in data.items():
+    ref.child(key).set(value)
+
+
+
+
+
+
+
+
